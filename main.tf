bbd35f38767ae3a6d4b81fd2f82a1353da6ded52
Nhat Vo <NhatVo@rpi.inc>
2024-01-07 14:38:53 -0800

    add AWS_REGION

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+  backend "s3" {
+    bucket = "keypair-1234"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+resource "aws_key_pair" "create_keypair" {
+  key_name = "tien08"
+  public_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCJgKe/Ko/3pXldymY2/bwP70xPJzP6NpKS9iQP450sWFtc5VuG3fqaOvGMzhrS3VUseALddYALiWmgf+cgjdvASeW3eIl54y2253QTme4eb1WOZ2/MvcANBuiJFqItL7SR4xlaAuI48yS8lh7L2OIIpzy8PUV+EcPD9KGKsAicFwIDAQAB"
+}
